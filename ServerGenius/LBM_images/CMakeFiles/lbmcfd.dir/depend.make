885354eac70e7dc82da2b9b3bd75ef09586da4bd
Ricky Massa <105085626+fmassa1@users.noreply.github.com>
2024-11-22 14:25:26 -0600

    Delete ServerGenius/LBM_images directory

--- ServerGenius/LBM_images/CMakeFiles/lbmcfd.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for lbmcfd.
-# This may be replaced when dependencies are built.
--- ServerGenius/LBM_images/timeout.sh
+++
@@ -1,7 +0,0 @@
-timeout=600
-echo Timeout: $timeout seconds until simulation shutdown
-echo ""
-sleep $timeout
-echo ""
-echo Timeout: $timeout seconds have elapsed, killing jupyter
-pkill jupyter-noteboo
